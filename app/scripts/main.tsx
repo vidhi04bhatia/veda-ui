93ab3146c4535736caa25770ac3fe4a8de967fc7
Gjore Milevski <dzole0311@gmail.com>
2024-06-12 17:01:59 +0200

    Install packages and peer deps

--- app/scripts/main.tsx
+++ app/scripts/main.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import { DevseedUiThemeProvider as DsTp } from '@devseed-ui/theme-provider';
 import { userPages } from 'veda';
 
+import '@trussworks/react-uswds/lib/index.css';
+
 import { discoveryRoutes } from './redirects';
 import theme, { GlobalStyles } from '$styles/theme';
 import { getAppURL } from '$utils/history';
