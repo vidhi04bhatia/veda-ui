f868ff965c4fc4810f55dce4e5aeaa432f0dcfe5
Gjore Milevski <dzole0311@gmail.com>
2025-01-03 05:22:52 +0100

    Remove encoder, switch siteAlert and banner to markdown

--- app/scripts/components/common/site-alert/index.tsx
+++ app/scripts/components/common/site-alert/index.tsx
@@ -62,7 +62,10 @@ export default function SiteAlert({
               type !== infoTypeFlag ? 'bg-secondary-lighter' : ''
             }`}
           >
-            <div dangerouslySetInnerHTML={{ __html: decode(content) }} />
+            <div
+              className='usa-alert__text'
+              dangerouslySetInnerHTML={{ __html: content }}
+            />
           </USWDSSiteAlert>
           <div className='position-absolute top-0 right-0 margin-right-3 height-full display-flex'>
             <button
